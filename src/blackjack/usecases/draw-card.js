aeb6a098c290e28e3b171e75ccb544192813bbd9
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-06-23 16:59:33 -0600

    Correcion de gramatica

--- src/blackjack/usecases/draw-card.js
+++ src/blackjack/usecases/draw-card.js
@@ -1,12 +1,12 @@
 /**
- * This function take a card of the deck
- * @param {Array<String>} deck is an Array of Strings
- * @returns {String} returns a card
+ * This function takes a card from the deck
+ * @param {Array<String>} deck - An array of strings representing the deck of cards
+ * @returns {String} The drawn card
  */
 
 export const drawCard = (deck) => {
-  if (!deck || !deck.length === 0) {
-    throw new Error("The deck doesnt have cards");
+  if (!deck || deck.length === 0) {
+    throw new Error("The deck doesn't  have cards");
   }
   return deck.pop();
 };
