70a0f3a6a7d4ce2239e0d2f356ddedcd258581d9
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-06-23 17:39:55 -0600

    Modulo de create-card

---
+++ src/blackjack/usecases/create-card.js
@@ -0,0 +1,12 @@
+/**
+ * This function creates and adds a card element to the player's card div
+ * @param {String} card - The card to create
+ * @param {Number} turn - The current turn of the player
+ * @param {Array<HTMLElement>} divPlayerCards - The array of HTML elements for player cards
+ */
+export const createCard = (card, turn, divPlayerCards) => {
+  const imgCard = document.createElement("img");
+  imgCard.src = `/assets/cartas/${card}.png`;
+  imgCard.classList.add("carta");
+  divPlayerCards[turn].append(imgCard);
+};
